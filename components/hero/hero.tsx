c1bc1f3421712cbc189e00d9021048a7d1774009
njoromyke <mykenjoroge01@gmail.com>
2023-04-08 12:44:13 +0300

    added social section

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Hero = () => {
@@ -13,7 +14,36 @@ const Hero = () => {
           <h2 className="text-lg">
             A fullstack software engineer with a passion for building scalable and performant web applications.
           </h2>
-          <button className="bg-indigo-500 hover:bg-indigo-600 text-white py-2 px-4 mt-4 rounded">Button</button>
+          <div className="flex space-x-4 mt-4">
+            <Link
+              target="_blank"
+              href="https://github.com/njoromyke"
+              className="h-10 w-10 bg-secondary text-primary flex justify-center items-center transition-all duration-500 ease-in-out rounded hover:rotate-45"
+            >
+              <i className="text-lg bi bi-github"></i>
+            </Link>
+            <Link
+              target="_blank"
+              href="https://www.linkedin.com/in/njoromyke"
+              className="h-10 w-10 bg-secondary text-blue-600 flex justify-center items-center transition-all duration-500 ease-in-out rounded hover:rotate-45"
+            >
+              <i className="text-lg bi bi-linkedin"></i>
+            </Link>
+            <Link
+              target="_blank"
+              href="https://wa.me/254729842998"
+              className="h-10 w-10 bg-secondary text-green-600 flex justify-center items-center  transition-all duration-500 ease-in-out rounded hover:rotate-45"
+            >
+              <i className="text-lg bi bi-whatsapp"></i>
+            </Link>
+            <Link
+              target="_blank"
+              href="mailto:mykenjoroge01@gmail.com"
+              className="h-10 w-10 bg-secondary text-red-600 flex justify-center items-center  transition-all duration-500 ease-in-out rounded hover:rotate-45"
+            >
+              <i className="text-lg bi bi-envelope-at"></i>
+            </Link>
+          </div>
         </div>
         <div className="w-1/2 flex justify-center items-center">
           <Image src={"/mike.png"} width={300} height={300} alt="Mike" />
