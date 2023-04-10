60952f4d751218616b7f9f2fc96f3a5def4f7225
njoromyke <mykenjoroge01@gmail.com>
2023-04-10 14:10:25 +0300

    added new SEO features, viewport and author

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,9 +19,11 @@ const Home = () => {
         />
         <meta
           name="keywords"
-          content={`Michael Njoroge, Software Engineer in Kenya, Web Developer in Kenya, Mobile Developer in Kenya, Software Development Portfolio Kenya, Web Development Portfolio Kenya, Mobile App Development Kenya, Full Stack Developer Kenya, Frontend Developer Kenya,Backend Developer Kenya
-Kenyan Software Developer, Kenyan Web Developer, Kenyan Mobile Developer, Kenyan Software Development Portfolio, Kenyan Web Development Portfolio, Kenyan Mobile App Development, Kenyan Full Stack Developer, Kenyan Frontend Developer,Kenyan Backend Developer,IT Professional in Kenya`}
+          content={`Michael Njoroge, Software Engineer in Kenya, Web Developer in Kenya, Mobile Developer in Kenya, Software Development Portfolio Kenya, Web Development Portfolio Kenya, Mobile App Development Kenya, Full Stack Developer Kenya, Frontend Developer Kenya,Backend Developer Kenya,IT Professional in Kenya,Kenyan Software Developer, Kenyan Web Developer, Kenyan Mobile Developer, Kenyan Software Development Portfolio, Kenyan Web Development Portfolio, Kenyan Mobile App Development, Kenyan Full Stack Developer, Kenyan Frontend Developer,Kenyan Backend Developer,IT Professional in Kenya`}
         />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="author" content="Michael Njoroge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />{" "}
       </Head>
       <Hero />
       <Skills />
